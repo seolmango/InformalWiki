52fefb0ac4cd8aee53fb9188242a312fea5bb6e6
Chaehwan Seol <seolchaehwan@naver.com>
2022-04-07 16:19:53 +0900

    기본적인 파일 구조 생성

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ let http = require('http').Server(app);
 let path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+let port = process.env.PORT || 3000;
+http.listen(port, function () {
+  console.log("server on!: http://localhost:3000");
+});
